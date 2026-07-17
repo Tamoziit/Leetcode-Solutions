5d301a39de0fef95019f9c9a9603486b2bdc5cfd
Tamoziit <tamojitdas181007@gmail.com>
2026-07-17 11:52:56 +0530

    LC:3312 - Sorted GCD Pair Queries

---
+++ Leetcode-Hard/SortedGCDPairQueries.java
@@ -0,0 +1,60 @@
+/**
+ * Inclusion-Exclusion Principle + Prefix Sum + Binary Search
+ * Time: O(mlogm + qlogm), where,
+ * - m: the maximum value in nums
+ * - q: the length of queries
+ * Space: O(m)
+ */
+public class SortedGCDPairQueries {
+
+    public int[] gcdValues(int[] nums, long[] queries) {
+        int m = 0;
+        for (int num : nums) {
+            m = Math.max(m, num);
+        }
+
+        long[] cnt = new long[m + 1];
+        for (int num : nums) {
+            cnt[num]++;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = i * 2; j <= m; j += i) {
+                cnt[i] += cnt[j];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            cnt[i] = (cnt[i] * (cnt[i] - 1)) / 2;
+        }
+
+        for (int i = m; i >= 1; i--) {
+            for (int j = i * 2; j <= m; j += i) {
+                cnt[i] -= cnt[j];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        int[] ans = new int[queries.length];
+        for (int k = 0; k < queries.length; k++) {
+            long q = queries[k] + 1;
+            int left = 1, right = m;
+
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (cnt[mid] >= q) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            ans[k] = left;
+        }
+
+        return ans;
+    }
+}

7a54a1e9d63cd803892008af029ca0a51a17930b
Tamoziit <tamojitdas181007@gmail.com>
2026-07-19 19:59:19 +0530

    LC:1081 - Smallest Subsequence of Distinct Characters

---
+++ Leetcode-Medium/SmallestSubsequenceOfDistinctChars.java
@@ -0,0 +1,37 @@
+/**
+ * Greedy + Monotonic Stack
+ * Time: O(N)
+ * Space: O(26) [const.]
+ */
+public class SmallestSubsequenceOfDistinctChars {
+
+    public String smallestSubsequence(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+
+            num[ch - 'a'] -= 1;
+        }
+
+        return sb.toString();
+    }
+}
